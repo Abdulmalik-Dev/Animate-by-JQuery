b4f92e3f69afd49183f12c3b9bc41cfe1e522381
Abdulmalik-Dev <abdulmalikkld325@gmail.com>
2021-03-17 00:31:29 +0300

    After add  commends

--- smallAnimate.js
+++ smallAnimate.js
@@ -1,12 +1,22 @@
 $(document).ready( function () {
+    // To hide the elements in load page 
     $("p , input , label , button").hide()
 
+    // To make slide of form in 1 second
     $("form").animate({
         "top":"100px",
     },1000)
+
+    // To appear paragraph element
     $("p").fadeIn(8000);
-    $("input").show(5000);
-    $("label").show(5000);
-    $("button").fadeIn(8000);
+
+    // To appear all inputs elements
+    $("input").show(3000);
+
+    // To appear all labels elements
+    $("label").show(3500);
+
+    // To appear login button 
+    $("button").fadeIn(4000);
     
 });
